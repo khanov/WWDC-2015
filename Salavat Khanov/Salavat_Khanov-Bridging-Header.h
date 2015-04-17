75fe7b1bd5d34a3dc65ddf4e7fe860e2d4311768
Salavat Khanov <salavat@khanov.com>
2015-04-17 14:36:56 +0500

    Change Work screen: slide left/right between pages.

---
+++ Salavat Khanov/Salavat_Khanov-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Salavat_Khanov-Bridging-Header.h
+//  Salavat Khanov
+//
+//  Created by Salavat Khanov on 4/17/15.
+//  Copyright (c) 2015 Arty Technology. All rights reserved.
+//
+
+#import "SLPagingViewController.h"
